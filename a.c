4cad6c2bb5d22df78499159bdde7bbd83d9e4804
cks9410 <cks9415@gmail.com>
2021-12-26 20:45:16 +0900

    MERGEgit add .!

--- a.c
+++ a.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-	printf("hello world!\n");
-	printf("hello\n");
-=======
 	printf("hello AAA\n");
 	printf("AAAAAAA\n");
 	printf("BBBBBB\n");
->>>>>>> NEW_TEST
 	return 0;
 }
